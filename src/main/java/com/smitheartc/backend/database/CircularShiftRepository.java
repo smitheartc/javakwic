6fa095a1647d20e4071af5acceddcc0cd882471c
Caleb Smitheart <smitheartc@gmail.com>
2025-11-26 17:39:03 -0600

    Implemented specific and searching

--- src/main/java/com/smitheartc/backend/database/CircularShiftRepository.java
+++ src/main/java/com/smitheartc/backend/database/CircularShiftRepository.java
@@ -2,12 +2,16 @@ package com.smitheartc.backend.database;
 
 import java.util.Collection;
 
+import com.smitheartc.backend.cyberminer.SearchHandler;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CircularShiftRepository extends CrudRepository<CircularShiftEntity, Integer> {
+public interface CircularShiftRepository extends JpaRepository<CircularShiftEntity, Integer> {
 
     // @Query("SELECT e.circularShift, LOWER(e.circularShift) AS lowercase FROM CircularShiftEntity e ORDER BY lowercase ASC")
     // Collection<String> getIndexTable();
@@ -17,4 +21,10 @@ public interface CircularShiftRepository extends CrudRepository<CircularShiftEnt
     Collection<String> getIndexTable();
 
 
+    // OR if you want to use a specific JPQL query:
+    @Query("SELECT e.circularShift, e.url FROM CircularShiftEntity e WHERE e.hash IN (SELECT c.hash FROM CircularShiftEntity c WHERE c.circularShift LIKE %:keyword%) AND e.shifted = FALSE")
+    Page<CircularShiftEntity> andSearch(
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
 }
